fc41afc404631548ee056cd241086be303294f0f
dblackmorris <blackdeath8765@gmail.com>
2016-09-10 19:22:03 -0400

    Add files via upload

---
+++ core/menu.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# BABY Framework Menu module
+# Created By D@nt3 (Ritik Dubey)
+# Email : blackdeath8765@Gmail.Com
+
+
+from core import wcolors
+
+def main_info():
+	ston = wcolors.color.BLUE + "[" + wcolors.color.ENDC
+	print "-------------------------------------------------------------------------------------------------------"
+	print "\t\t" + ston + wcolors.color.GREEN + "BABY MITM Framework       " + wcolors.color.ENDC         +            "]"
+	print "\t\t" + ston + "Author :" + wcolors.color.RED + "RITIK DUBEY #D@nt3" + wcolors.color.ENDC   +            "]"
+	print "\t\t" + ston + "Codename :" + wcolors.color.PURPLE + "B@bY_Sp1d3r     " + wcolors.color.ENDC    +         "]"
+	print "\t\t" + ston + "Available Modules : " + wcolors.color.GREEN + "7     " + wcolors.color.ENDC +             "]"
+	print "\t\t" + ston + "Update Date : [" + wcolors.color.RED + "11.9.2016  " + wcolors.color.ENDC + "]"
+	print "\t\t" + ston + "" + wcolors.color.BLUE + "blackdeath8765@gmail.com" + wcolors.color.ENDC + "  ]"
+	print "\n\n"
